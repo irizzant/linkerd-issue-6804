123aabfff2b6cf5c0d72c68646dd3dcebc1282d6
irizzant <i.rizzante@gmail.com>
2021-09-19 00:06:55 +0200

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+k3d cluster create --k3s-server-arg --disable=traefik --wait
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
+helm repo update
+
+kubectl create ns monitoring
+
+linkerd install | kubectl apply -f-
+
+linkerd viz install | kubectl apply -f-
+
+kubectl annotate ns monitoring linkerd.io/inject=enabled
+
+kubectl -n monitoring apply -f thanos-objstore-config.yaml
+
+helm install prometheus -n monitoring -f values-prometheus.yaml --version 17.1.0 prometheus-community/kube-prometheus-stack
+
+helm install thanos --values=values-thanos.yaml --namespace monitoring bitnami/thanos
